264402a322939885e513dbe989353cfd686814f9
Matheus Bento <matheusbentodepereira@gmail.com>
2022-07-14 09:25:48 -0300

    Create project and install firebase

---
+++ angular14-firebase/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular14-firebase/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular14-firebase',
+    appId: '1:336189787726:web:43ff088cfebac1d4c4bd1f',
+    storageBucket: 'angular14-firebase.appspot.com',
+    apiKey: 'AIzaSyDrH3bKuQgTYbAMEsuR4Ki3goxaqiN_w4U',
+    authDomain: 'angular14-firebase.firebaseapp.com',
+    messagingSenderId: '336189787726',
+  },
+  production: true
+};
