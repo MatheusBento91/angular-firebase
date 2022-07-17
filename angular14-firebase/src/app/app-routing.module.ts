12dff94a5e31b65122137d4bb8b79ed2f4acc379
Matheus Bento <matheusbentodepereira@gmail.com>
2022-07-16 21:32:06 -0300

    Create auth.guard and protect the routes - create new components for access denied and not found - deploy in firebase

--- angular14-firebase/src/app/app-routing.module.ts
+++ angular14-firebase/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/components/login/login.component';
+import { AuthGuard } from './services/auth.guard';
+import { AccessDeniedComponent } from './shared/components/access-denied/access-denied.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard] },
 
-  { path: '**', component: LoginComponent},
+  { path: 'access-denied', component: AccessDeniedComponent},
+  { path: '**', component: NotFoundComponent},
 
 ];
 
