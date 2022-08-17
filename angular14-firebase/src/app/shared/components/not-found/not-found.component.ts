39366d3f5c9def261a7c0e7d7a8cd309a6670f20
Matheus Bento <matheusbentodepereira@gmail.com>
2022-08-17 15:33:33 -0300

    html and css of acess-denied and not-found

--- angular14-firebase/src/app/shared/components/not-found/not-found.component.ts
+++ angular14-firebase/src/app/shared/components/not-found/not-found.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
+  title = 'Ops! Pagina não encontrada';
 
   constructor() { }
 
