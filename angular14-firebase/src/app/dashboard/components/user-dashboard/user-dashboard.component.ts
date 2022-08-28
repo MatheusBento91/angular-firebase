86ff31589c67a9e05382ad400deae9bda3f22ee3
Matheus Bento <matheusbentodepereira@gmail.com>
2022-08-28 18:55:23 -0300

    refactoring code in user-dashboard

--- angular14-firebase/src/app/dashboard/components/user-dashboard/user-dashboard.component.ts
+++ angular14-firebase/src/app/dashboard/components/user-dashboard/user-dashboard.component.ts
@@ -38,6 +38,17 @@ export class UserDashboardComponent implements OnInit {
     },
   ];
 
+  fullStack: number[] = [];
+  fullStackCounter: number[] = [];
+  backEnd: number[] = [];
+  backEndCounter: number[] = [];
+  frontEnd: number[] = [];
+  frontEndCounter: number[] = [];
+  mobile: number[] = [];
+  mobileCounter: number[] = [];
+  devOps: number[] = [];
+  devOpsCounter: number[] = [];
+
   //bar
   public barChartData: ChartConfiguration<'bar'>['data'] = {
     datasets: [],
@@ -52,8 +63,8 @@ export class UserDashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUsersPrincipalStack();
+    this.getUsersAverageAndRangeSalary();
     this.getUsersLevel();
-    this.getUsersSalary();
   }
 
   doughnutChartClicked(e: any) {}
@@ -179,366 +190,146 @@ export class UserDashboardComponent implements OnInit {
     });
   }
 
-  getUsersSalary() {
+  getUsersAverageAndRangeSalary() {
     this.userService.get().subscribe((data) => {
       if (data) {
-        let fullStack: any[] = [];
-        let fullStackCounter: any[] = [];
-        let backEnd: any[] = [];
-        let backEndCounter: any[] = [];
-        let frontEnd: any[] = [];
-        let frontEndCounter: any[] = [];
-        let mobile: any[] = [];
-        let mobileCounter: any[] = [];
-        let devOps: any[] = [];
-        let devOpsCounter: any[] = [];
-
-        data.forEach((item: any, index) => {
+        data.forEach((item: any) => {
           switch (item.principalStack) {
             case PrincipalStack.FULLTSACK:
-              if (item.salaryExpectation < 5000) {
-                if (fullStack[0] != undefined)
-                  fullStack[0] = fullStack[0] + item.salaryExpectation;
-                else fullStack[0] = item.salaryExpectation;
-
-                if (fullStackCounter[0] != undefined)
-                  fullStackCounter[0] = fullStackCounter[0] + 1;
-                else fullStackCounter[0] = 1;
-              } else if (
-                item.salaryExpectation > 5000 &&
-                item.salaryExpectation < 10000
-              ) {
-                if (fullStack[1] != undefined)
-                  fullStack[1] = fullStack[1] + item.salaryExpectation;
-                else fullStack[1] = item.salaryExpectation;
-
-                if (fullStackCounter[1] != undefined)
-                  fullStackCounter[1] = fullStackCounter[1] + 1;
-                else fullStackCounter[1] = 1;
-              } else if (
-                item.salaryExpectation > 10000 &&
-                item.salaryExpectation < 15000
-              ) {
-                if (fullStack[2] != undefined)
-                  fullStack[2] = fullStack[2] + item.salaryExpectation;
-                else fullStack[2] = item.salaryExpectation;
-
-                if (fullStackCounter[2] != undefined)
-                  fullStackCounter[2] = fullStackCounter[2] + 1;
-                else fullStackCounter[2] = 1;
-              } else if (
-                item.salaryExpectation > 15000 &&
-                item.salaryExpectation < 20000
-              ) {
-                if (fullStack[3] != undefined)
-                  fullStack[3] = fullStack[3] + item.salaryExpectation;
-                else fullStack[3] = item.salaryExpectation;
-
-                if (fullStackCounter[3] != undefined)
-                  fullStackCounter[3] = fullStackCounter[3] + 1;
-                else fullStackCounter[3] = 1;
-              } else if (item.salaryExpectation > 2000) {
-                if (fullStack[4] != undefined)
-                  fullStack[4] = fullStack[4] + item.salaryExpectation;
-                else fullStack[4] = item.salaryExpectation;
-
-                if (fullStackCounter[4] != undefined)
-                  fullStackCounter[4] = fullStackCounter[4] + 1;
-                else fullStackCounter[4] = 1;
-              }
-
+              this.setAvarageAndRangeSalary(item, this.fullStack, this.fullStackCounter);
               break;
             case PrincipalStack.BACKEND:
-              if (item.salaryExpectation < 5000) {
-                if (backEnd[0] != undefined)
-                  backEnd[0] = backEnd[0] + item.salaryExpectation;
-                else backEnd[0] = item.salaryExpectation;
-
-                if (backEndCounter[0] != undefined)
-                  backEndCounter[0] = backEndCounter[0] + 1;
-                else backEndCounter[0] = 1;
-              } else if (
-                item.salaryExpectation > 5000 &&
-                item.salaryExpectation < 10000
-              ) {
-                if (backEnd[1] != undefined)
-                  backEnd[1] = backEnd[1] + item.salaryExpectation;
-                else backEnd[1] = item.salaryExpectation;
-
-                if (backEndCounter[1] != undefined)
-                  backEndCounter[1] = backEndCounter[1] + 1;
-                else backEndCounter[1] = 1;
-              } else if (
-                item.salaryExpectation > 10000 &&
-                item.salaryExpectation < 15000
-              ) {
-                if (backEnd[2] != undefined)
-                  backEnd[2] = backEnd[2] + item.salaryExpectation;
-                else backEnd[2] = item.salaryExpectation;
-
-                if (backEndCounter[2] != undefined)
-                  backEndCounter[2] = backEndCounter[2] + 1;
-                else backEndCounter[2] = 1;
-              } else if (
-                item.salaryExpectation > 15000 &&
-                item.salaryExpectation < 20000
-              ) {
-                if (backEnd[3] != undefined)
-                  backEnd[3] = backEnd[3] + item.salaryExpectation;
-                else backEnd[3] = item.salaryExpectation;
-
-                if (backEndCounter[3] != undefined)
-                  backEndCounter[3] = backEndCounter[3] + 1;
-                else backEndCounter[3] = 1;
-              } else if (item.salaryExpectation > 2000) {
-                if (backEnd[4] != undefined)
-                  backEnd[4] = backEnd[4] + item.salaryExpectation;
-                else backEnd[4] = item.salaryExpectation;
-
-                if (backEndCounter[4] != undefined)
-                  backEndCounter[4] = backEndCounter[4] + 1;
-                else backEndCounter[4] = 1;
-              }
-
+              this.setAvarageAndRangeSalary(item, this.backEnd, this.backEndCounter);
               break;
             case PrincipalStack.FRONTEND:
-              if (item.salaryExpectation < 5000) {
-                if (frontEnd[0] != undefined)
-                  frontEnd[0] = frontEnd[0] + item.salaryExpectation;
-                else frontEnd[0] = item.salaryExpectation;
-
-                if (frontEndCounter[0] != undefined)
-                  frontEndCounter[0] = frontEndCounter[0] + 1;
-                else frontEndCounter[0] = 1;
-              } else if (
-                item.salaryExpectation > 5000 &&
-                item.salaryExpectation < 10000
-              ) {
-                if (frontEnd[1] != undefined)
-                  frontEnd[1] = frontEnd[1] + item.salaryExpectation;
-                else frontEnd[1] = item.salaryExpectation;
-
-                if (frontEndCounter[1] != undefined)
-                  frontEndCounter[1] = frontEndCounter[1] + 1;
-                else frontEndCounter[1] = 1;
-              } else if (
-                item.salaryExpectation > 10000 &&
-                item.salaryExpectation < 15000
-              ) {
-                if (frontEnd[2] != undefined)
-                  frontEnd[2] = frontEnd[2] + item.salaryExpectation;
-                else frontEnd[2] = item.salaryExpectation;
-
-                if (frontEndCounter[2] != undefined)
-                  frontEndCounter[2] = frontEndCounter[2] + 1;
-                else frontEndCounter[2] = 1;
-              } else if (
-                item.salaryExpectation > 15000 &&
-                item.salaryExpectation < 20000
-              ) {
-                if (frontEnd[3] != undefined)
-                  frontEnd[3] = frontEnd[3] + item.salaryExpectation;
-                else frontEnd[3] = item.salaryExpectation;
-
-                if (frontEndCounter[3] != undefined)
-                  frontEndCounter[3] = frontEndCounter[3] + 1;
-                else frontEndCounter[3] = 1;
-              } else if (item.salaryExpectation > 2000) {
-                if (frontEnd[4] != undefined)
-                  frontEnd[4] = frontEnd[4] + item.salaryExpectation;
-                else frontEnd[4] = item.salaryExpectation;
-
-                if (frontEndCounter[4] != undefined)
-                  frontEndCounter[4] = frontEndCounter[4] + 1;
-                else frontEndCounter[4] = 1;
-              }
-
+              this.setAvarageAndRangeSalary(item, this.frontEnd, this.frontEndCounter);
               break;
             case PrincipalStack.MOBILE:
-              if (item.salaryExpectation < 5000) {
-                if (mobile[0] != undefined)
-                  mobile[0] = mobile[0] + item.salaryExpectation;
-                else mobile[0] = item.salaryExpectation;
-
-                if (mobileCounter[0] != undefined)
-                  mobileCounter[0] = mobileCounter[0] + 1;
-                else mobileCounter[0] = 1;
-              } else if (
-                item.salaryExpectation > 5000 &&
-                item.salaryExpectation < 10000
-              ) {
-                if (mobile[1] != undefined)
-                  mobile[1] = mobile[1] + item.salaryExpectation;
-                else mobile[1] = item.salaryExpectation;
-
-                if (mobileCounter[1] != undefined)
-                  mobileCounter[1] = mobileCounter[1] + 1;
-                else mobileCounter[1] = 1;
-              } else if (
-                item.salaryExpectation > 10000 &&
-                item.salaryExpectation < 15000
-              ) {
-                if (mobile[2] != undefined)
-                  mobile[2] = mobile[2] + item.salaryExpectation;
-                else mobile[2] = item.salaryExpectation;
-
-                if (mobileCounter[2] != undefined)
-                  mobileCounter[2] = mobileCounter[2] + 1;
-                else mobileCounter[2] = 1;
-              } else if (
-                item.salaryExpectation > 15000 &&
-                item.salaryExpectation < 20000
-              ) {
-                if (mobile[3] != undefined)
-                  mobile[3] = mobile[3] + item.salaryExpectation;
-                else mobile[3] = item.salaryExpectation;
-
-                if (mobileCounter[3] != undefined)
-                  mobileCounter[3] = mobileCounter[3] + 1;
-                else mobileCounter[3] = 1;
-              } else if (item.salaryExpectation > 2000) {
-                if (mobile[4] != undefined)
-                  mobile[4] = mobile[4] + item.salaryExpectation;
-                else mobile[4] = item.salaryExpectation;
-
-                if (mobileCounter[4] != undefined)
-                  mobileCounter[4] = mobileCounter[4] + 1;
-                else mobileCounter[4] = 1;
-              }
-
+              this.setAvarageAndRangeSalary(item, this.mobile, this.mobileCounter);
               break;
             case PrincipalStack.DEVOPS:
-              if (item.salaryExpectation < 5000) {
-                if (devOps[0] != undefined)
-                  devOps[0] = devOps[0] + item.salaryExpectation;
-                else devOps[0] = item.salaryExpectation;
-
-                if (devOpsCounter[0] != undefined)
-                  devOpsCounter[0] = devOpsCounter[0] + 1;
-                else devOpsCounter[0] = 1;
-              } else if (
-                item.salaryExpectation > 5000 &&
-                item.salaryExpectation < 10000
-              ) {
-                if (devOps[1] != undefined)
-                  devOps[1] = devOps[1] + item.salaryExpectation;
-                else devOps[1] = item.salaryExpectation;
-
-                if (devOpsCounter[1] != undefined)
-                  devOpsCounter[1] = devOpsCounter[1] + 1;
-                else devOpsCounter[1] = 1;
-              } else if (
-                item.salaryExpectation > 10000 &&
-                item.salaryExpectation < 15000
-              ) {
-                if (devOps[2] != undefined)
-                  devOps[2] = devOps[2] + item.salaryExpectation;
-                else devOps[2] = item.salaryExpectation;
-
-                if (devOpsCounter[2] != undefined)
-                  devOpsCounter[2] = devOpsCounter[2] + 1;
-                else devOpsCounter[2] = 1;
-              } else if (
-                item.salaryExpectation > 15000 &&
-                item.salaryExpectation < 20000
-              ) {
-                if (devOps[3] != undefined)
-                  devOps[3] = devOps[3] + item.salaryExpectation;
-                else devOps[3] = item.salaryExpectation;
-
-                if (devOpsCounter[3] != undefined)
-                  devOpsCounter[3] = devOpsCounter[3] + 1;
-                else devOpsCounter[3] = 1;
-              } else if (item.salaryExpectation > 2000) {
-                if (devOps[4] != undefined)
-                  devOps[4] = devOps[4] + item.salaryExpectation;
-                else devOps[4] = item.salaryExpectation;
-
-                if (devOpsCounter[4] != undefined)
-                  devOpsCounter[4] = devOpsCounter[4] + 1;
-                else devOpsCounter[4] = 1;
-              }
-
+              this.setAvarageAndRangeSalary(item, this.devOps, this.devOpsCounter);
               break;
           }
         });
 
-        fullStack.forEach((element: any, index) => {
-          fullStack[index] = element / fullStackCounter[index];
+        this.fullStack.forEach((element: any, index) => {
+          this.fullStack[index] = element / this.fullStackCounter[index];
         });
 
-        backEnd.forEach((element: any, index) => {
-          backEnd[index] = element / backEndCounter[index];
+        this.backEnd.forEach((element: any, index) => {
+          this.backEnd[index] = element / this.backEndCounter[index];
         });
 
-        frontEnd.forEach((element: any, index) => {
-          frontEnd[index] = element / frontEndCounter[index];
+        this.frontEnd.forEach((element: any, index) => {
+          this.frontEnd[index] = element / this.frontEndCounter[index];
         });
 
-        mobile.forEach((element: any, index) => {
-          mobile[index] = element / mobileCounter[index];
+        this.mobile.forEach((element: any, index) => {
+          this.mobile[index] = element / this.mobileCounter[index];
         });
 
-        devOps.forEach((element: any, index) => {
-          devOps[index] = element / devOpsCounter[index];
+        this.devOps.forEach((element: any, index) => {
+          this.devOps[index] = element / this.devOpsCounter[index];
         });
 
-        const barChartData: ChartConfiguration<'bar'>['data'] = {
+        this.barChartData = {
           labels: ['0 5k', '5k 10k', '10k 15k', '15k 20k', '20k >'],
           datasets: [
             {
-              data: fullStack,
-              label: 'Full Stack',
+              data: this.fullStack,
+              label: PrincipalStack.FULLTSACK,
             },
             {
-              data: backEnd,
-              label: 'Back-End',
+              data: this.backEnd,
+              label: PrincipalStack.BACKEND,
             },
             {
-              data: frontEnd,
-              label: 'Front-End',
+              data: this.frontEnd,
+              label: PrincipalStack.FRONTEND,
             },
             {
-              data: mobile,
-              label: 'Mobile',
+              data: this.mobile,
+              label: PrincipalStack.MOBILE,
             },
             {
-              data: devOps,
-              label: 'DevOps',
+              data: this.devOps,
+              label: PrincipalStack.DEVOPS,
             },
           ],
         };
 
-        const barChartSalaryRangeData: ChartConfiguration<'bar'>['data'] = {
+        this.barChartSalaryRangeData = {
           labels: ['0 5k', '5k 10k', '10k 15k', '15k 20k', '20k >'],
           datasets: [
             {
-              data: fullStackCounter,
-              label: 'Full Stack',
+              data: this.fullStackCounter,
+              label: PrincipalStack.FULLTSACK,
             },
             {
-              data: backEndCounter,
-              label: 'Back-End',
+              data: this.backEndCounter,
+              label: PrincipalStack.BACKEND,
             },
             {
-              data: frontEndCounter,
-              label: 'Front-End',
+              data: this.frontEndCounter,
+              label: PrincipalStack.FRONTEND,
             },
             {
-              data: mobileCounter,
-              label: 'Mobile',
+              data: this.mobileCounter,
+              label: PrincipalStack.MOBILE,
             },
             {
-              data: devOpsCounter,
-              label: 'DevOps',
+              data: this.devOpsCounter,
+              label: PrincipalStack.DEVOPS,
             },
           ],
         };
 
-        this.barChartData = barChartData;
-        this.barChartSalaryRangeData = barChartSalaryRangeData;
         this.loading = false;
       }
     });
+  }
+
+  setAvarageAndRangeSalary(item: any, stack: any, stackCounter: any) {
+    if (item.salaryExpectation < 5000) {
+      if (stack[0] != undefined) stack[0] = stack[0] + item.salaryExpectation;
+      else stack[0] = item.salaryExpectation;
+
+      if (stackCounter[0] != undefined) stackCounter[0] = stackCounter[0] + 1;
+      else stackCounter[0] = 1;
+    } else if (
+      item.salaryExpectation > 5000 &&
+      item.salaryExpectation < 10000
+    ) {
+      if (stack[1] != undefined) stack[1] = stack[1] + item.salaryExpectation;
+      else stack[1] = item.salaryExpectation;
+
+      if (stackCounter[1] != undefined) stackCounter[1] = stackCounter[1] + 1;
+      else stackCounter[1] = 1;
+    } else if (
+      item.salaryExpectation > 10000 &&
+      item.salaryExpectation < 15000
+    ) {
+      if (stack[2] != undefined) stack[2] = stack[2] + item.salaryExpectation;
+      else stack[2] = item.salaryExpectation;
+
+      if (stackCounter[2] != undefined) stackCounter[2] = stackCounter[2] + 1;
+      else stackCounter[2] = 1;
+    } else if (
+      item.salaryExpectation > 15000 &&
+      item.salaryExpectation < 20000
+    ) {
+      if (stack[3] != undefined) stack[3] = stack[3] + item.salaryExpectation;
+      else stack[3] = item.salaryExpectation;
+
+      if (stackCounter[3] != undefined) stackCounter[3] = stackCounter[3] + 1;
+      else stackCounter[3] = 1;
+    } else if (item.salaryExpectation > 2000) {
+      if (stack[4] != undefined) stack[4] = stack[4] + item.salaryExpectation;
+      else stack[4] = item.salaryExpectation;
+
+      if (stackCounter[4] != undefined) stackCounter[4] = stackCounter[4] + 1;
+      else stackCounter[4] = 1;
+    }
   }
 }
